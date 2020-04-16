5c1a6c6be87f377f14d7304aaf71a366b823c2b4
unknown <db@teamtroxler.com>
2020-04-16 02:09:12 -0400

    Big O considerations, combos up to 4

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# Dictionary-Mixer" 
+"Scramble some words up
+
+Created this to practice working with different Python data structures
+
+I ACCEPT NO LIABILITY FOR ANY UNETHICAL USAGE" 
--- mix.py
+++ mix.py
@@ -4,36 +4,69 @@ x = []
 for start in file:
 	x.append(start.strip('\n'))
 
-def mix(c, b):
-	if c in each:
-		x.append(each.replace(c,b))
-		x.append(each.replace(c,b,1))
-		x.append(each.replace(c,b,2))
-		re = each[::-1]
-		abc = re.replace(c,b)
-		x.append(abc[::-1])
-		abc = re.replace(c,b,1)
-		x.append(abc[::-1])
-		abc = re.replace(c,b,2)
-		x.append(abc[::-1])
+def mix(c, b, each):
 
+	if each.count(c) == 4:
+		xyz2 = int(0)
+		while xyz2 < each.count(c):
+			x.append(each.replace(c,b,xyz2))
+			xyz2 += 1
+		
+		re = each[::-1]
+		xyz = int(1)
+		while xyz < each.count(c):
+			abc = re.replace(c,b,xyz)
+			x.append(abc[::-1])
+			xyz += 1
+
+		s1 = each.split(c)
+		x.append(s1[0] + c + s1[1] + c + s1[2] + b + s1[3] + c + s1[4])
+		x.append(s1[0] + c + s1[1] + b + s1[2] + c + s1[3] + c + s1[4])
+		x.append(s1[0] + c + s1[1] + b + s1[2] + c + s1[3] + b + s1[4])
+		x.append(s1[0] + c + s1[1] + b + s1[2] + b + s1[3] + c + s1[4])
+		x.append(s1[0] + b + s1[1] + c + s1[2] + c + s1[3] + b + s1[4])
+		x.append(s1[0] + b + s1[1] + c + s1[2] + b + s1[3] + c + s1[4])
+		x.append(s1[0] + b + s1[1] + c + s1[2] + b + s1[3] + b + s1[4])
+		x.append(s1[0] + b + s1[1] + b + s1[2] + c + s1[3] + b + s1[4])
+
+	if each.count(c) == 3:
+		xyz2 = int(0)
+		while xyz2 < each.count(c):
+			x.append(each.replace(c,b,xyz2))
+			xyz2 += 1
+		
+		re = each[::-1]
+		xyz = int(1)
+		while xyz < each.count(c):
+			abc = re.replace(c,b,xyz)
+			x.append(abc[::-1])
+			xyz += 1
+
+		s1 = each.split(c)
+		x.append(s1[0] + b + s1[1] + c + s1[2] + b + s1[3])
+		x.append(s1[0] + c + s1[1] + b + s1[2] + c + s1[3])
+	
+	if each.count(c) == 2:
+			x.append(each.replace(c,b,1))
+			x.append(each.replace(c,b,2))
+			re = each[::-1]
+			abc = re.replace(c,b,1)
+			x.append(abc[::-1])
+	
+	if each.count(c) == 1:
+		x.append(each.replace(c,b,1))
 
 mixes = {'o': '0', 'e':'3', 'l':'1', 'i':'1', 'a':'@', 's':'$'}
 
 for k, v in mixes.items():
-	for each in set(x):
-		mix(k,v)
+	for item in set(x):
+		mix(k,v, item)
 
-print(set(x))
-print(len(set(x)))
+print(x)
+print(len(x))
 print("unique values generated from orignal")
 print(len(file))
-
 
 new = open('new.txt','r+')
 for yo in set(x):
 	new.write(yo + '\n')
-
-
-
-
